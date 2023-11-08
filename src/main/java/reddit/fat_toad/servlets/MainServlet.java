495bc61ab4094fb7a381062ba5e5412637885d39
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-11-08 17:29:49 +0200

    Start dev my reddit.

---
+++ src/main/java/reddit/fat_toad/servlets/MainServlet.java
@@ -0,0 +1,17 @@
+package reddit.fat_toad.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        //req.setAttribute("page-body", "_main.jsp");
+        req.getRequestDispatcher("WEB-INF/_main.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/_main.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Fat toad</title>
+</head>
+<body>
+    <p>Start dev my reddit</p>
+</body>
+</html>
