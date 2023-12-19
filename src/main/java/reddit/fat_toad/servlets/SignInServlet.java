046b44c01469eb5923509771838a1e8d8f7beb91
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-12-20 01:18:33 +0200

    Implementation registration.

--- src/main/java/reddit/fat_toad/servlets/SignInServlet.java
+++ src/main/java/reddit/fat_toad/servlets/SignInServlet.java
@@ -1,10 +1,13 @@
 package reddit.fat_toad.servlets;
 
+import com.google.gson.Gson;
 import com.google.inject.Singleton;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.Console;
 import java.io.IOException;
 
 @Singleton
@@ -17,5 +20,33 @@ public class SignInServlet extends HttpServlet
         req.setAttribute("ShowFooter", true);
         req.setAttribute("page-body", "sign_in.jsp");
         req.getRequestDispatcher("WEB-INF/_layout.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+/*        StringBuilder buffer = new StringBuilder();
+        BufferedReader reader = req.getReader();
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+        }
+
+        String data = buffer.toString();
+        System.out.println("Received data: " + data);
+
+        // Теперь вы можете использовать библиотеку JSON (например, Gson) для преобразования строки JSON в объект
+        // Пример с использованием Gson:
+        Gson gson = new Gson();
+        YourClassType yourObject = gson.fromJson(data, YourClassType.class);
+
+        String email = yourObject.getEmail();
+        String nickname = yourObject.getNickname();
+        String password = yourObject.getPassword();
+
+        System.out.println("Email: " + email);
+        System.out.println("Nickname: " + nickname);
+        System.out.println("Password: " + password);*/
     }
 }
--- src/main/webapp/js/registration_authorization.js
+++ src/main/webapp/js/registration_authorization.js
@@ -10,5 +10,101 @@ function ShowSignInContent()
     document.querySelector('.registration-block').style.display = 'none';
 }
 
+function Send()
+{
+    const formData = {
+        email: document.getElementById("id-input-email").value,
+        nickname: document.getElementById("id-input-nickname").value,
+        password: document.getElementById("id-input-password").value
+    };
+
+    fetch("/FatToad/signin", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("Received data:", data);
+            // Дополнительная обработка успешного ответа от сервера
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            // Обработка ошибки при запросе к серверу
+        });
+}
+
+
+
+function NeuenBenutzerSenden()
+{
+    event.preventDefault();
+
+    const email = document.getElementById("id-input-email").value;
+    const nickname = document.getElementById("id-input-nickname").value;
+    const password = document.getElementById("id-input-password").value;
+
+    if (Inspector(email, nickname, password))
+    {
+        Send();
+
+        alert("👽 Welcome\nWe are glad that you are with us, we promise that we will not use your data for personal gain. "
+            + "Just visit the site, share your impressions, communicate, and become our critic in the gaming industry");
+
+        location.reload();
+    }
+}
+
+function Inspector(email, nickname, password)
+{
+    const error_email_message = document.getElementById("id-error-email-message");
+    const error_nickname_message = document.getElementById("id-error-nickname-message");
+    const error_password_message = document.getElementById("id-error-password-message");
+
+    if (email.trim() === "")
+    {
+        error_email_message.textContent = "You have an empty line!";
+        return false;
+    }
+    else if (!email.includes("@gmail.com"))
+    {
+        error_email_message.textContent = "This doesn't look like mail";
+        return false;
+    }
+    else
+        error_email_message.textContent = "";
+
+    if (nickname.trim() === "")
+    {
+        error_nickname_message.textContent = "You have an empty line!";
+        return false;
+    }
+    else
+        error_nickname_message.textContent = "";
+
+    if (password.trim() === "")
+    {
+        error_password_message.textContent = "You have an empty line!";
+        return false;
+    }
+    else if(password.length < 6)
+    {
+        error_password_message.textContent = "The password has less than 6 characters!"
+        return false;
+    }
+    else
+        error_nickname_message.textContent = "";
+
+    return true;
+}
+
 document.getElementById("id-sign-up").onclick = ShowSignUpContent;
 document.getElementById("id-sign-in").onclick = ShowSignInContent;
+document.getElementById("id-signup-button").addEventListener("click", NeuenBenutzerSenden);
